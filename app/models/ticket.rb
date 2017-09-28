3a83dd1c51ee5f7e9d3d5d2037f457304c226c52
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-09-28 09:08:43 -0400

    add validation and more associations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
+  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_xtation_id 
+  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
+  
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates 
 end
