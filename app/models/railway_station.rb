cc8c1b2af60f5bcd01cef65b3898f5ee82449d98
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-10-07 22:48:35 -0400

    view stations on the route order by station position

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,8 +1,10 @@
 class RailwayStation < ActiveRecord::Base
   has_many :trains, foreign_key: :current_station_id, dependent: :nullify
   # has_and_belongs_to_many :routes
-  has_many :railway_stations_routes
+  has_many :railway_stations_routes, dependent: :destroy
   has_many :routes, through: :railway_stations_routes
   
   validates :title, presence: true
+  
+  scope :ordered, -> { joins(:railway_stations_routes).order('railway_stations_routes.station_serial_number').uniq }
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,7 @@
 class Route < ActiveRecord::Base
   has_many :trains, dependent: :nullify
   # has_and_belongs_to_many :railway_stations
-  has_many :railway_stations_routes
+  has_many :railway_stations_routes, dependent: :destroy
   has_many :railway_stations, through: :railway_stations_routes
   
   validates :name, presence: true
@@ -21,5 +21,9 @@ class Route < ActiveRecord::Base
     if railway_stations.size < 2
       errors.add(:base, "Route should contain at least 2 stations")
     end
+  end
+  
+  def stations_on_route
+    railway_stations.where().ordered
   end
 end
