ab58606197ca4a93feb83096f1b227cb7e3e8d3e
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-10-18 22:00:41 -0400

    LESSON-17: connect train and cars via routes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,13 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
+  before_action :set_train
 
   def index
     @cars = Car.all
   end
 
   def show
+  
   end
 
   def new
@@ -13,10 +15,10 @@ class CarsController < ApplicationController
   end
 
   def create
-    @car = Car.new(car_params)
-
+    # @car = Car.new(car_params)
+    @car = @train.cars.new(car_params)
     if @car.save
-      redirect_to @car
+      redirect_to @train
     else
       render :new
     end
@@ -42,6 +44,10 @@ class CarsController < ApplicationController
   
 
   private
+  
+  def set_train
+    @train = Train.find(params[:train_id]) if params[:train_id]
+  end
 
   def set_car
     @car = Car.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :routes
-  resources :trains
+  resources :trains do
+    resources :cars, shallow: true
+    resources :econom_car, controller: 'cars', type: 'EconomCar', shallow: true
+    resources :sv_car, controller: 'cars', type: 'SvCar', shallow: true
+    resources :business_car, controller: 'cars', type: 'BusinessCar', shallow: true
+    resources :seated_car, controller: 'cars', type: 'SeatedCar', shallow: true
+  end
   resources :railway_stations do
     patch :update_serial_number, on: :member #we can use on: :collectoin
   end
