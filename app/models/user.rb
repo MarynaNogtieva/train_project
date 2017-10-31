f4ff3d23c94f56842d44fdfc41efa8cd19add8c8
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-10-30 22:01:43 -0400

    LESSON-17: apply fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :tickets, dependent: :destroyxx
+  has_many :tickets, dependent: :destroy
   validates :first_name, :last_name, :email, presence: true
 end
