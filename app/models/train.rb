71c87caf7fbf37ccbc51439b61fcdcc072848323
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-10-31 22:20:27 -0400

    LESSON-17: remove comment

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,5 @@
 class Train < ApplicationRecord
   belongs_to :route, optional: true
-  # belongs_to :railway_station
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   
   has_many :tickets, dependent: :destroy
