90e7bbacb2e37d67bf6b8ce01486d3c111b563c5
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-06 12:25:02 -0500

    LESSON-18 add redirect to pages on login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  get 'welcome/index'
-  root  "welcome#index"
-  
   resource :search, only: [:show, :new]
   resources :routes
   resources :tickets
@@ -21,6 +18,9 @@ Rails.application.routes.draw do
   
     resources :routes
     resources :tickets
+    root 'welcome#index'
   end
+
+  root  "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
