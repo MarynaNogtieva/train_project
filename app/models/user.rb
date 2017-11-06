b13337ff27ab404de4115e0922440aa21dcfc0fe
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-05 21:37:53 -0500

    LESSON-18 add dispay of user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :tickets, dependent: :destroy
   validates :first_name, :last_name, :email, presence: true
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
