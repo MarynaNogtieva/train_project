f9542c87096bcd63d1319df2b27360db0b91d6a6
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-14 21:36:19 -0500

    Lesson-21 add jquery for railway_stations

---
+++ app/assets/javascripts/railway_stations.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('a.edit_station').click(function(e) {
+    e.preventDefault();
+    var station_id;
+    var form;
+    var title;
+    
+    stationId = $(this).data('stationId');
+    form = $('#edit_railway_station_' + stationId);
+    title = $('#railway_station_title_' + stationId)
+    
+    if (!$(this).hasClass('cancel')) {
+      $(this).html('Cancel').addClass('cancel');
+    } else {
+      $(this).html('Edit').removeClass('cancel');
+    }
+    
+    form.toggle();
+    title.toggle();
+  });
+});
